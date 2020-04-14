734467941d8fbbecde6a2ff358d3c66b7d8cc888
Elise <elise@web-id.fr>
2020-04-14 15:44:13 +0200

    Add nova views

---
+++ packages/webid/cms/src/resources/views/vendor/nova/router.blade.php
@@ -0,0 +1,28 @@
+@extends('nova::layout')
+
+@section('content')
+    <div>
+        <div class="w1/2">
+            <select id="select-language-translatable"
+                    class="w-full form-control form-select"
+                    style="width: auto; position: relative; top: -4px; margin-bottom: 10px;"
+                    onchange="Nova.$emit('change-language', this.value);"
+            >
+                @php
+                    $langs = \Illuminate\Support\Arr::wrap(\Webid\Cms\Src\App\Facades\LanguageFacade::getUsedLanguage());
+                    ksort($langs);
+                @endphp
+                @foreach($langs as $langCode => $langName)
+                    <option value="{{ $langCode }}">{{ $langName }}</option>
+                @endforeach
+            </select>
+        </div>
+        <loading ref="loading"></loading>
+
+        <transition name="fade" mode="out-in">
+            <router-view :key="$route.name + ($route.params.resourceName || '')"></router-view>
+        </transition>
+
+        <portal-target name="modals" multiple></portal-target>
+    </div>
+@endsection
