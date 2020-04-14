d97cff67d444fbf7e10345d8d6dcdc623e2a013b
Elise <elise@web-id.fr>
2020-04-14 15:47:12 +0200

    Add translatable config

---
+++ packages/webid/cms/src/config/translatable.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | If a translation has not been set for a given locale,
+     | use this locale instead.
+     |--------------------------------------------------------------------------
+     */
+
+    'fallback_locale' => null,
+
+    /*
+     |--------------------------------------------------------------------------
+     | Liste des langues disponibles par tous les sites
+     |--------------------------------------------------------------------------
+     */
+
+    'locales' => [
+        'en' => 'English',
+        'fr' => 'Français',
+        'it' => 'Italiano',
+        'es' => 'Español',
+        'pt' => 'Português',
+        'ja' => '日本語',
+        'zh' => '中文',
+        'ru' => 'Русский',
+        'ar' => 'العربية',
+        'de' => 'Deutsch',
+        'pt-br' => 'Português brasileiro',
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Liste des langues dont le sens de lecture est Right-To-Left
+     |--------------------------------------------------------------------------
+     */
+
+    'rtl_locales' => [
+        'ar',
+    ],
+];
