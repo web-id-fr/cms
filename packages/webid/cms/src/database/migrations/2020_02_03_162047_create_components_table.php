e4f417cfe3f3d7a9477f9336ba28cd11b487128d
Elise <elise@web-id.fr>
2020-04-14 17:25:48 +0200

    Add template migrations

---
+++ packages/webid/cms/src/database/migrations/2020_02_03_162047_create_components_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('components', function (Blueprint $table) {
+            $table->integer('template_id');
+            $table->integer('component_id');
+            $table->string('component_type');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('components');
+    }
+}
