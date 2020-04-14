a084db267a3536c1900ce80bd508aec0a866532d
Elise <elise@web-id.fr>
2020-04-14 17:26:17 +0200

    Add component config

---
+++ packages/webid/cms/src/config/components.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Components config
+|--------------------------------------------------------------------------
+|
+| Use the following schema to store components :
+|
+| ComponentModel::class => [
+|   'title" => 'Title component",
+|   'image' => 'PATH_TO_COMPONENT_IMAGE', // /images/components/component.png
+|   'resource => CompenentResource::class,
+|   'view' => 'PATH_TO_COMPONENT_VIEW, // components/component
+|   'nova' => 'URL_TO_ACCES_RESOURCE_NOVA', // /nova/resources/component
+| ]
+*/
+
+return [
+
+];
