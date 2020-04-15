b34fb2b83fa0d09702a315a28f7f45142f5173c7
Elise <elise@web-id.fr>
2020-04-15 17:48:10 +0200

    Add FielditemField

---
+++ packages/webid/cms/src/nova-components/FieldItemField/src/FieldServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Webid\FieldItemField;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('FieldItemField', __DIR__.'/../dist/js/field.js');
+            Nova::style('FieldItemField', __DIR__.'/../dist/css/field.css');
+        });
+    }
+}
