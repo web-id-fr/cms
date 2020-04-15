5d7171f974df818798dffb5b5d126f5b03d3211b
Elise <elise@web-id.fr>
2020-04-15 17:45:55 +0200

    Add forms view

---
+++ packages/webid/cms/src/resources/views/forms/fields/date.blade.php
@@ -0,0 +1,5 @@
+<div class="line_form">
+    <input type="date" name="{{ data_get($field, 'field_name') }}" @if( data_get($field, 'required')) required @endif>
+    <div class="field-error">
+    </div>
+</div>
