5c9738e327e060e083343b83bd4e5d1da8f72ee9
Elise <elise@web-id.fr>
2020-04-20 12:54:29 +0200

    Add nova-components MenuTool

---
+++ src/nova-components/MenuTool/src/ToolServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Webid\MenuTool;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            //
+        });
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
