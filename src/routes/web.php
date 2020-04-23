733bde1e6ef85d478273888ebeb5d337eaf211c7
Elise <elise@web-id.fr>
2020-04-23 11:38:55 +0200

    Fix form routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Webid\Cms\Src\App\Http\Middleware\CheckLanguageExist;
 use Webid\Cms\Src\App\Http\Middleware\Language;
+use Spatie\Honeypot\ProtectAgainstSpam;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ Route::get('/', 'Webid\Cms\Src\App\Http\Controllers\TemplateController@rootPage'
 Route::group([
     'namespace' => 'Webid\Cms\Src\App\Http\Controllers',
     'prefix' => '{lang}',
-    'middleware' => [Language::class, CheckLanguageExist::class],
+    'middleware' => ['web', Language::class, CheckLanguageExist::class],
 ], function () {
     // Homepage
     Route::get('/', 'TemplateController@index')->name('home');
@@ -33,8 +34,8 @@ Route::group([
 
 Route::group([
     'prefix' => 'form',
-    'namespace' => 'Modules\Ajax\Form',
-    'middleware' => ['web', 'antispam']
+    'namespace' => 'Webid\Cms\Src\App\Http\Controllers\Modules\Ajax\Form',
+    'middleware' => ['web', ProtectAgainstSpam::class]
 ], function () {
-    Route::post('/send', 'FormController@sendForm')->name('send.form');
+    Route::post('/send', 'FormController@handle')->name('send.form');
 });
