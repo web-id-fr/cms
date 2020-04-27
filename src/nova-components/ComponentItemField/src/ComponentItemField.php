bafc05c452ab2384c45f9da55e68aabb0b33105b
Elise <elise@web-id.fr>
2020-04-27 16:43:27 +0200

    Fix ComponentitemField

--- src/nova-components/ComponentItemField/src/ComponentItemField.php
+++ src/nova-components/ComponentItemField/src/ComponentItemField.php
@@ -12,6 +12,9 @@ use Webid\Cms\Src\App\Repositories\Components\NewsletterComponentRepository;
 
 class ComponentItemField extends Field
 {
+    protected $galleryComponentRepository;
+    protected $newsletterComponentRepository;
+
     /**
      * The field's component.
      *
@@ -28,25 +31,22 @@ class ComponentItemField extends Field
      */
     public function __construct(string $name, ?string $attribute = null, ?mixed $resolveCallback = null)
     {
-        $galleryComponentRepository = app()->make(GalleryComponentRepository::class);
-        $newsletterComponentRepository = app()->make(NewsletterComponentRepository::class);
-        $allComponent = collect();
+        $this->galleryComponentRepository = app(GalleryComponentRepository::class);
+        $this->newsletterComponentRepository = app(NewsletterComponentRepository::class);
+        $allComponents = collect();
 
-        // GALLERIES
-        $allGalleryComponents = $galleryComponentRepository->getPublishedComponents();
-        $allGalleryComponents = $this->mapItems($allGalleryComponents, GalleryComponent::class);
-        $allGalleryComponents->each(function ($gallery_component) use (&$allComponent) {
-            $allComponent->push($gallery_component);
-        });
+        $this->loadComponents(
+            $this->galleryComponentRepository->getPublishedComponents(),
+            GalleryComponent::class,
+            $allComponents
+        );
+        $this->loadComponents(
+            $this->newsletterComponentRepository->getPublishedComponents(),
+            NewsletterComponent::class,
+            $allComponents
+        );
 
-        // NEWSLETTERS
-        $allNewsletterComponents = $newsletterComponentRepository->getPublishedComponents();
-        $allNewsletterComponents = $this->mapItems($allNewsletterComponents,NewsletterComponent::class);
-        $allNewsletterComponents->each(function ($newsletter_component) use (&$allComponent) {
-            $allComponent->push($newsletter_component);
-        });
-
-        $this->withMeta(['items' => $allComponent]);
+        $this->withMeta(['items' => $allComponents]);
         parent::__construct($name, $attribute, $resolveCallback);
     }
 
@@ -67,6 +67,23 @@ class ComponentItemField extends Field
     }
 
     /**
+     * @param $publishComponent
+     * @param $model
+     * @param $allComponents
+     *
+     * @return mixed
+     */
+    protected function loadComponents($publishComponent, $model, $allComponents)
+    {
+        $allPublishComponents = $this->mapItems($publishComponent, $model);
+        $allPublishComponents->each(function ($component) use (&$allComponents) {
+            $allComponents->push($component);
+        });
+
+        return $allComponents;
+    }
+
+    /**
      * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @param $requestAttribute
      * @param $model
@@ -80,19 +97,16 @@ class ComponentItemField extends Field
         $galleryComponentIds = [];
         $newsletterComponentIds = [];
 
-        $components->each(function (
-            $component,
-            $key
-        ) use (
-            &$galleryComponentIds,
-            &$newsletterComponentIds
-        ) {
-            if ($component['component_type'] == GalleryComponent::class) {
-                $galleryComponentIds[$component['id']] = ['order' => $key + 1];
-            } elseif ($component['component_type'] == NewsletterComponent::class) {
-                $newsletterComponentIds[$component['id']] = ['order' => $key + 1];
+        foreach ($components as $component => $key) {
+            switch ($component['component_type']) {
+                case GalleryComponent::class:
+                    $galleryComponentIds[$component['id']] = ['order' => $key + 1];
+                    break;
+                case NewsletterComponent::class:
+                    $newsletterComponentIds[$component['id']] = ['order' => $key + 1];
+                    break;
             }
-        });
+        }
 
         Template::saved(function ($model) use (
             $galleryComponentIds,
