b6d050a5536ec99b6cbd8d7be5c5f75030846c14
Elise <elise@web-id.fr>
2020-04-29 18:59:56 +0200

    Add placeholder field for NewsletterComponent

---
+++ src/database/migrations/2020_04_29_165603_add_placeholder_field_for_newsletters_component_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlaceholderFieldForNewslettersComponentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('newsletters_component', function(Blueprint $table) {
+            $table->json('placeholder')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('newsletters_component', function (Blueprint $table) {
+            $table->dropColumn('placeholder');
+        });
+    }
+}
