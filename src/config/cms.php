3a89228f65f11fc341a5fd93c57cb93fc1b14276
Elise <elise@web-id.fr>
2020-04-30 12:29:25 +0200

    Fix url for s3

--- src/config/cms.php
+++ src/config/cms.php
@@ -4,10 +4,10 @@ return [
     'template_model' => \Webid\Cms\Src\App\Models\Template::class,
     // FOR MODULES GALLERIES
     'gallery_path' => 's3' == env('FILESYSTEM_DRIVER')
-        ? 'https://' . env('AWS_BUCKET') . '/' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/Galeries'
+        ? 'https://' . env('AWS_BUCKET') . '.s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/Galeries'
         : storage_path('app/public/Galeries/'),
     // FOR IMAGES IN NOVA-COMPONENTS
     'image_path' =>'s3' == env('FILESYSTEM_DRIVER')
-        ? 'https://' . env('AWS_BUCKET') . '/' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/'
+        ? 'https://' . env('AWS_BUCKET') . '.s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/'
         : "/",
 ];
