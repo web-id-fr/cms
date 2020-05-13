96d40254c8d4f0d259cb127e3455ff8c975249ca
Elise <elise@web-id.fr>
2020-05-13 10:56:54 +0200

    Add GalleryServiceContract

---
+++ src/app/Services/Contracts/GalleryServiceContract.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Webid\Cms\Src\App\Services\Contracts;
+
+interface GalleryServiceContract
+{
+    /**
+     * @return array
+     */
+    public function getGalleries(): array;
+}
