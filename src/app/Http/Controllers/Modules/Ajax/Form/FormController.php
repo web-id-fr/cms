1320288825e1686bad820955d49446248e3bbe4a
Elise <elise@web-id.fr>
2020-06-08 14:42:46 +0200

    Update FormController

--- src/app/Http/Controllers/Modules/Ajax/Form/FormController.php
+++ src/app/Http/Controllers/Modules/Ajax/Form/FormController.php
@@ -3,34 +3,29 @@
 namespace Webid\Cms\Src\App\Http\Controllers\Modules\Ajax\Form;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 use Webid\Cms\Src\App\Http\Requests\FormRequest;
 use Webid\Cms\Src\App\Mail\SendForm;
 use Webid\Cms\Src\App\Repositories\Modules\Form\FormRepository;
 use Webid\Cms\Src\App\Repositories\Modules\Form\ServiceRepository;
-use Webid\Cms\Src\App\Services\MailService;
 
 class FormController extends Controller
 {
-    /** @var MailService  */
-    protected $mailService;
-
-    /** @var ServiceRepository  */
+    /** @var ServiceRepository */
     protected $serviceRepository;
 
-    /** @var FormRepository  */
+    /** @var FormRepository */
     protected $formRepository;
 
     /**
-     * @param MailService $mailService
      * @param ServiceRepository $serviceRepository
      * @param FormRepository $formRepository
      */
     public function __construct(
-        MailService $mailService,
         ServiceRepository $serviceRepository,
         FormRepository $formRepository
-    ) {
-        $this->mailService = $mailService;
+    )
+    {
         $this->serviceRepository = $serviceRepository;
         $this->formRepository = $formRepository;
     }
@@ -50,7 +45,7 @@ class FormController extends Controller
             $to = $form->recipients->pluck("email");
         }
 
-        $this->mailService->sendForm(new SendForm($request->all()), $to ?? config('mail.from.address'));
+        Mail::to($to ?? config('mail.from.address'))->send(new SendForm($request->all()));
 
         return response()->json([
             'errors' => false,
