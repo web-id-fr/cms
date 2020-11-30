7ab272097545331f2ad5d4b7b9a15d6e9acbe32b
Elise <elise@web-id.fr>
2020-11-30 15:24:25 +0100

    Translate Slideshow

--- src/app/Nova/Modules/Slideshow/Slideshow.php
+++ src/app/Nova/Modules/Slideshow/Slideshow.php
@@ -49,18 +49,18 @@ class Slideshow extends Resource
         return [
             ID::make()->sortable(),
 
-            Translatable::make('Title')
+            Translatable::make(__('Title'))
                 ->singleLine()
                 ->rules('required'),
 
-            Boolean::make('Arrows display', 'js_controls'),
+            Boolean::make(__('Arrows display'), 'js_controls'),
 
-            Boolean::make('Automatic slider', 'js_animate_auto'),
+            Boolean::make(__('Automatic slider'), 'js_animate_auto'),
 
-            Number::make('Sliding speed', 'js_speed')
+            Number::make(__('Sliding speed'), 'js_speed')
                 ->min(1)
                 ->step(1)
-                ->help('By default 5 seconds')
+                ->help(__('By default 5 seconds'))
                 ->resolveUsing(function ($js_speed) {
                     if(empty($js_speed)) {
                         return '';
@@ -70,7 +70,7 @@ class Slideshow extends Resource
                     return $js_speed / 1000;
                 }),
 
-            ImageItemField::make('Slides')
+            ImageItemField::make(__('Slides'))
                 ->onlyOnForms(),
         ];
     }
