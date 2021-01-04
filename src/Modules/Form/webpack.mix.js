404bdbc60814d84ea72753925d1118752daa3bbf
Elise <elise@web-id.fr>
2021-01-04 14:41:47 +0100

    :wastebasket: delete unnecessary files

--- src/Modules/Form/webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
-
-const moduleName = 'form';
-const modulePublicPath = 'modules/' + moduleName;
-const publicFolderPath = '../../../../../../resources';
-
-mix.copyDirectory(__dirname + '/Resources/js', publicFolderPath + '/' + modulePublicPath + '/js');
