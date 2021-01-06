33c05b7536cfd431775bf8433a69c3dd8bdf8bf3
Wilfried Jourdil <wilfried@web-id.fr>
2021-01-06 16:37:41 +0100

    👷 Try to make package composer v2-compliant

--- src/app/Repositories/Modules/Galleries/GalleryRepository.php
+++ src/app/Repositories/Modules/Galleries/GalleryRepository.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Webid\Cms\App\Repositories;
+namespace Webid\Cms\App\Repositories\Modules\Galleries;
 
 use Webid\Cms\App\Models\Modules\Galleries\Gallery;
+use Webid\Cms\App\Repositories\BaseRepository;
 
 class GalleryRepository extends BaseRepository
 {
