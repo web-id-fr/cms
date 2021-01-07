ef572015bc0e443bb3840ce779d34b03f4c75a14
Elise <elise@web-id.fr>
2021-01-07 20:37:36 +0100

    :white_check_mark: Delete wrong test

--- src/Modules/Form/Tests/Feature/FormTest.php
+++ src/Modules/Form/Tests/Feature/FormTest.php
@@ -3,8 +3,6 @@
 namespace Webid\Cms\Modules\Form\Tests\Feature;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Mail;
-use Webid\Cms\Modules\Form\Mail\SendForm;
 use Webid\Cms\Modules\Form\Tests\FormTestCase;
 use Webid\Cms\Modules\Form\Tests\Helpers\FormCreator;
 
@@ -30,17 +28,5 @@ class FormTest extends FormTestCase
         return $this->createForm([
             'recipient_type' => 2
         ]);
-    }
-
-    /** @test */
-    public function we_can_send_contact_form_with_required_information()
-    {
-        Mail::fake();
-
-        $this->ajaxPost(route(self::_FORM_ROUTE, [
-            'lang' => 'fr'
-        ]))->assertSuccessful();
-
-        Mail::assertSent(SendForm::class);
     }
 }
