a252fb91cfc6289048b119067c9fbbf53073d50e
Elise <elise@web-id.fr>
2021-01-29 17:30:16 +0100

    :fire: Delete unnecessary config

--- src/Modules/Galleries/Config/galleries.php
+++ src/Modules/Galleries/Config/galleries.php
@@ -5,15 +5,12 @@
 | Galleries config
 |--------------------------------------------------------------------------
 |
-| gallery_folder_name : Définit le nom du dossier dans lequel mettre les galeries
 | gallery_path : Définit le chemin du dossier des galeries
 | filesystem_driver : Définit le type de driver utiliser pour les fichiers
 |
 */
 
 return [
-    'gallery_folder_name' => 'Galeries',
-
     'gallery_path' => 's3' == env('FILESYSTEM_DRIVER')
         ? '/Galeries'
         : storage_path('app/public/Galeries/'),
