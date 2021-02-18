6c628fbd75922b954635267b1af99c5f54a4187f
Elise <elise@web-id.fr>
2021-02-18 15:22:13 +0100

    :lipstick: Add better design for FieldItemField

--- src/nova-components/FieldItemField/resources/js/components/DetailField.vue
+++ src/nova-components/FieldItemField/resources/js/components/DetailField.vue
@@ -1,11 +1,34 @@
 <template>
     <panel-item :field="field">
         <template slot="value">
-            <div class="w-1/2 shadow-md p-3 my-2"
+            <div class="relative flex bg-white mb-2 pb-1"
                  v-for="item in field.value"
                  :key="item.id"
             >
-                {{ selectFirstTitle(item.title, item.formable_type) }}
+                <div class="z-10 bg-white border-t border-l border-b border-60 h-auto pin-l pin-t rounded-l self-start w-8">
+                    <div>
+                        <button disabled type="button" class="group-control btn border-t border-r border-40 w-8 h-8 bloc">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 20 20" stroke="grey">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"/>
+                            </svg>
+                        </button>
+                        <button disabled type="button" class="group-control btn border-t border-r border-40 w-8 h-8 block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 20 20" stroke="grey">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
+                            </svg>
+                        </button>
+                        <button disabled  class="group-control btn border-t border-r border-40 w-8 h-8 block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 20 20" aria-labelledby="delete" role="presentation" class="fill-current">
+                                <path fill="grey" fill-rule="nonzero" d="M6 4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6H1a1 1 0 1 1 0-2h5zM4 6v12h12V6H4zm8-2V2H8v2h4zM8 8a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="flex min-h-full w-full border-60 border rounded-r-lg">
+                    <div class="flex-auto self-center text-center py-2 m-2">
+                        <p class="break-words">{{ selectFirstTitle(item.title, item.formable_type) }}</p>
+                    </div>
+                </div>
             </div>
         </template>
     </panel-item>
--- src/nova-components/FieldItemField/resources/js/components/FormField.vue
+++ src/nova-components/FieldItemField/resources/js/components/FormField.vue
@@ -19,12 +19,34 @@
 
                 <div class="flex flex-col mt-3">
                     <draggable v-model="selected" ghost-class="ghost">
-                        <div
-                            class="shadow-md p-3 my-2"
-                            v-for="element in selected"
-                            :key="element.id"
+                        <div class="relative flex bg-white mb-2 pb-1"
+                             v-for="element in selected"
+                             :key="element.id"
                         >
-                            {{ selectFirstTitle(element.title, element.formable_type) }}
+                            <div class="z-10 bg-white border-t border-l border-b border-60 h-auto pin-l pin-t rounded-l self-start w-8">
+                                <div>
+                                    <button v-tooltip.click="__('Move up')" type="button" class="group-control btn border-t border-r border-40 w-8 h-8 bloc has-tooltip" @click.prevent="moveUp(element)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 20 20" stroke="grey">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"/>
+                                        </svg>
+                                    </button>
+                                    <button v-tooltip.click="__('Move down')" type="button" class="group-control btn border-t border-r border-40 w-8 h-8 block has-tooltip" @click.prevent="moveDown(element)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 20 20" stroke="grey">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
+                                        </svg>
+                                    </button>
+                                    <button v-tooltip.click="__('Detach')" @click.prevent="deselectComponents(element)" class="group-control btn border-t border-r border-40 w-8 h-8 block has-tooltip">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 20 20" aria-labelledby="delete" role="presentation" class="fill-current">
+                                            <path fill="grey" fill-rule="nonzero" d="M6 4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6H1a1 1 0 1 1 0-2h5zM4 6v12h12V6H4zm8-2V2H8v2h4zM8 8a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="flex min-h-full w-full border-60 border rounded-r-lg">
+                                <div class="flex-auto self-center text-center py-2 m-2">
+                                    <p class="break-words">{{ selectFirstTitle(element.title, element.formable_type) }}</p>
+                                </div>
+                            </div>
                         </div>
                     </draggable>
                 </div>
@@ -87,6 +109,31 @@
              */
             handleChange(value) {
                 this.value = value;
+            },
+
+            moveUp(field) {
+                let findIndex = _.findIndex(this.selected, field);
+
+                if (findIndex <= 0) return;
+
+                this.selected.splice(findIndex - 1, 0, this.selected.splice(findIndex, 1)[0]);
+                console.log(this.selected);
+            },
+
+            moveDown(field) {
+                let findIndex = _.findIndex(this.selected, field);
+
+                if (findIndex < 0 || findIndex >= this.selected.length - 1) return;
+
+                this.selected.splice(findIndex + 1, 0, this.selected.splice(findIndex, 1)[0]);
+            },
+
+            deselectComponents(field) {
+                const findIndex = _.findIndex(this.selected, field);
+
+                if (findIndex >= 0) {
+                    this.selected.splice(findIndex, 1);
+                }
             },
 
             customLabel({title, formable_type}) {
