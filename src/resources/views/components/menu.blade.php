a8880ec7272a84c958042255e120310c8f7990de
Wilfried Jourdil <wilfried@web-id.fr>
2021-02-19 17:30:04 +0100

    :fire: Remove unused variables from menu template (#52)

--- src/resources/views/components/menu.blade.php
+++ src/resources/views/components/menu.blade.php
@@ -6,15 +6,8 @@
  | Les variables disponibles sont :
  |  • menu_items        un array, la liste des items du menu actuel
  |  • title             une string, le titre du menu (traduisible)
- |  • wrap              optionnel, une string, le nom de la balise qui contient le lien
- |  • title_wrap        optionnel, une string, contient la balise dans laquelle on affiche le titre,
- |                          une valeur vide signifie qu'on affiche pas le titre
  |
 --}}
-{{-- Si besoin, on affiche le titre du menu --}}
-@if(!empty($title_wrap) && is_string($title_wrap))
-    {!! "<{$title_wrap}>" !!}{{ "$title" }}{!! "</{$title_wrap}>" !!}
-@endif
 
 @foreach($menu_items as $item)
     {{-- On définit la variable $link_url selon le type d'item --}}
@@ -23,9 +16,6 @@
     @elseif(!empty($item['slug']))
         @php $link_url = route('pageFromSlug', [ 'slug' => data_get($item, 'slug', '') ]); @endphp
     @endif
-
-    {{-- Si on a une balise parente, on l'ouvre ici --}}
-    @if(!empty($wrap)) {!! "<{$wrap}>" !!} @endif
 
     {{-- Si on a un lien ET un titre, on affiche le lien --}}
     @if(!empty($link_url) && !empty(data_get($item, 'title')))
@@ -50,9 +40,6 @@
             @endif
         </div>
     @endif
-
-    {{-- Si on a une balise parente, on la ferme ici --}}
-    @if(!empty($wrap)) {!! "</{$wrap}>" !!} @endif
 
     {{-- On reset la variable $link_url --}}
     @php $link_url = ''; @endphp
