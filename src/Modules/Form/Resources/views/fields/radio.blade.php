d514e8b733c7efc9009d6f773f28ae13ccd9a547
Elise <elise@web-id.fr>
2021-03-11 16:12:18 +0100

    :art: Add conditions for fields

--- src/Modules/Form/Resources/views/fields/radio.blade.php
+++ src/Modules/Form/Resources/views/fields/radio.blade.php
@@ -1,11 +1,13 @@
 <div class="line_form">
     @foreach(data_get($field, 'field_options') as $option)
-        <label>
-            <input type="radio" name="{{ data_get($field, 'field_name') }}"
-                   value="{{ data_get($option, "attributes.option.$currentLangKey") }}"
-                   @if( data_get($field, 'required')) required @endif>
-            {{ data_get($option, "attributes.option.$currentLangKey") }}
-        </label>
+        @if(!empty(data_get($option, "option.$currentLangKey")))
+            <label>
+                <input type="radio" name="{{ data_get($field, 'field_name') }}"
+                       value="{{ data_get($option, "option.$currentLangKey") }}"
+                       @if( data_get($field, 'required')) required @endif>
+                {{ data_get($option, "option.$currentLangKey") }}
+            </label>
+        @endif
     @endforeach
     <div class="field-error">
     </div>
