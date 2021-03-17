4309c6fbfa077e672596ecbbe90b5fae7c15d6d9
Elise <elise@web-id.fr>
2021-03-17 16:04:13 +0100

    :bug: Fix migration name

--- src/database/migrations/2021_03_17_115845_add_meta_keywords_field_in_templates_table.php
+++ src/database/migrations/2021_03_17_115845_add_meta_keywords_field_in_templates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMetaDescriptionFieldInTemplatesTable extends Migration
+class AddMetaKeywordsFieldInTemplatesTable extends Migration
 {
     /**
      * Run the migrations.
