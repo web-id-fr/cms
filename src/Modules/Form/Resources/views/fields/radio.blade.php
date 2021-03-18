aed7d49a27b42c6422b159ab6ac37d53c1ee9553
Wilfried <wilfried@web-id.fr>
2021-03-18 18:09:50 +0100

    :bug: Fix radio input template

--- src/Modules/Form/Resources/views/fields/radio.blade.php
+++ src/Modules/Form/Resources/views/fields/radio.blade.php
@@ -1,14 +1,20 @@
 <div class="line_form">
-    @foreach(data_get($field, 'field_options') as $option)
-        @if(!empty(data_get($option, "option.$currentLangKey")))
-            <label>
-                <input type="radio" name="{{ data_get($field, 'field_name') }}"
-                       value="{{ data_get($option, "option.$currentLangKey") }}"
-                       @if( data_get($field, 'required')) required @endif>
-                {{ data_get($option, "option.$currentLangKey") }}
-            </label>
-        @endif
-    @endforeach
+    @if(!empty(data_get($field, 'field_options')))
+        @foreach(data_get($field, 'field_options') as $option)
+            @if(!empty(data_get($option, "item.$currentLangKey")))
+                <div class="option-container">
+                    <input type="radio"
+                           name="{{ data_get($field, 'field_name') }}"
+                           id="{{ data_get($field, 'field_name') . '-' . $loop->index }}"
+                           value="{{ data_get($option, "option.$currentLangKey") }}"
+                           @if( data_get($field, 'required')) required @endif>
+                    <label for="{{ data_get($field, 'field_name') . '-' . $loop->index }}" class="input-label">
+                        {{ data_get($option, "item.$currentLangKey") }}
+                    </label>
+                </div>
+            @endif
+        @endforeach
+    @endif
     <div class="field-error">
     </div>
 </div>
