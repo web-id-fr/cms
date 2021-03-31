7aba5b5b6c6289ec699d1dafba2b6542cb2346d9
Elise <elise@web-id.fr>
2021-03-31 17:31:00 +0200

    :sparkles: Add possibility to shorten text in index view

--- src/nova-components/TranslatableTool/resources/js/components/IndexField.vue
+++ src/nova-components/TranslatableTool/resources/js/components/IndexField.vue
@@ -1,6 +1,12 @@
 <template>
-    <div v-if="field.asHtml" v-html="value"></div>
-    <span v-else class="whitespace-no-wrap">{{ value }}</span>
+    <div v-if="field.shortenText">
+        <div v-if="field.asHtml" v-html="value.substr(0, 50) + '...'"></div>
+        <span v-else class="whitespace-no-wrap">{{ value.substr(0, 50) + '...' }}</span>
+    </div>
+    <div v-else>
+        <div v-if="field.asHtml" v-html="value"></div>
+        <span v-else class="whitespace-no-wrap">{{ value }}</span>
+    </div>
 </template>
 
 <script>
