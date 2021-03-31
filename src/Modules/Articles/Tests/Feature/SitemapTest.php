72d7fd0dbb6b637d91cdc6ea2c79e64a359cf808
Wilfried <wilfried@web-id.fr>
2021-03-31 18:29:39 +0200

    :white_check_mark: Update tests

--- src/Modules/Articles/Tests/Feature/SitemapTest.php
+++ src/Modules/Articles/Tests/Feature/SitemapTest.php
@@ -8,12 +8,14 @@ use Webid\Cms\Modules\Articles\Tests\ArticlesTestCase;
 use Webid\Cms\Modules\Articles\Tests\Helpers\ArticleCategoryCreator;
 use Webid\Cms\Modules\Articles\Tests\Helpers\ArticleCreator;
 use Webid\Cms\Tests\Helpers\Traits\LanguageCreator;
+use Webid\Cms\Tests\Helpers\Traits\TemplateCreator;
 
 class SitemapTest extends ArticlesTestCase
 {
     use ArticleCreator,
         ArticleCategoryCreator,
-        LanguageCreator;
+        LanguageCreator,
+        TemplateCreator;
 
     public function setUp(): void
     {
@@ -23,22 +25,16 @@ class SitemapTest extends ArticlesTestCase
 
         $this->createLanguage(['name' => 'Français', 'flag' => 'fr']);
         $this->createLanguage(['name' => 'English', 'flag' => 'en']);
-    }
-
-    /** @test */
-    public function sitemap_contains_articles_index_in_all_langs()
-    {
-        $xml = $this->generateSitemap();
-
-        $this->assertStringContainsString('<loc>https://localhost/fr/articles</loc>', $xml);
-        $this->assertStringContainsString('<loc>https://localhost/en/articles</loc>', $xml);
-        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles" />', 2);
-        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles" />', 2);
+        $this->createLanguage(['name' => '日本語', 'flag' => 'ja']);
     }
 
     /** @test */
     public function sitemap_contains_articles_and_categories_urls()
     {
+        $this->createPublicTemplate([
+            'contains_articles_list' => true,
+            'slug' => ['fr' => 'ma-page', 'en' => 'my-page'],
+        ]);
         $this->createPublishedArticle(['slug' => ['fr' => 'titre-article', 'en' => 'article-title']]);
         $this->createArticleCategory(['name' => ['fr' => 'nom-categorie', 'en' => 'category-name']]);
 
@@ -49,10 +45,34 @@ class SitemapTest extends ArticlesTestCase
         $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles/titre-article" />', 2);
         $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/article-title" />', 2);
 
-        $this->assertStringContainsString('<loc>https://localhost/fr/articles/categories/nom-categorie</loc>', $xml);
-        $this->assertStringContainsString('<loc>https://localhost/en/articles/categories/category-name</loc>', $xml);
-        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles/categories/nom-categorie" />', 2);
-        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/categories/category-name" />', 2);
+        $this->assertStringContainsString('<loc>https://localhost/fr/ma-page?category=nom-categorie</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/en/my-page?category=category-name</loc>', $xml);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/ma-page?category=nom-categorie" />', 2);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/my-page?category=category-name" />', 2);
+    }
+
+    /** @test */
+    public function sitemap_contains_articles_and_categories_urls_when_page_is_homepage()
+    {
+        $this->createPublicTemplate([
+            'homepage' => true,
+            'contains_articles_list' => true,
+            'slug' => ['fr' => 'ma-page', 'en' => 'my-page'],
+        ]);
+        $this->createPublishedArticle(['slug' => ['fr' => 'titre-article', 'en' => 'article-title']]);
+        $this->createArticleCategory(['name' => ['fr' => 'nom-categorie', 'en' => 'category-name']]);
+
+        $xml = $this->generateSitemap();
+
+        $this->assertStringContainsString('<loc>https://localhost/fr/articles/titre-article</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/en/articles/article-title</loc>', $xml);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles/titre-article" />', 2);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/article-title" />', 2);
+
+        $this->assertStringContainsString('<loc>https://localhost/fr?category=nom-categorie</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/en?category=category-name</loc>', $xml);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr?category=nom-categorie" />', 2);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en?category=category-name" />', 2);
     }
 
     /** @test */
@@ -78,6 +98,26 @@ class SitemapTest extends ArticlesTestCase
 
         $this->assertStringNotContainsString('<loc>https://localhost/en/articles/tags/nom-categorie</loc>', $xml);
         $this->assertStringNotContainsString('<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/tags/nom-categorie" />', $xml);
+    }
+
+    /** @test */
+    public function alternates_are_well_handled_for_multiple_articles_in_sitemap()
+    {
+        $this->createPublishedArticle(['slug' => ['fr' => 'article-1-fr']]);
+        $this->createPublishedArticle(['slug' => ['fr' => 'article-2-fr', 'en' => 'article-2-en']]);
+        $this->createPublishedArticle(['slug' => ['en' => 'article-3-en']]);
+
+        $xml = $this->generateSitemap();
+
+        $this->assertStringContainsString('<loc>https://localhost/fr/articles/article-1-fr</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/fr/articles/article-2-fr</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/en/articles/article-2-en</loc>', $xml);
+        $this->assertStringContainsString('<loc>https://localhost/en/articles/article-3-en</loc>', $xml);
+
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles/article-1-fr" />', 1);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="fr" href="https://localhost/fr/articles/article-2-fr" />', 2);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/article-2-en" />', 2);
+        $this->assertStringContainsStringTimes($xml, '<xhtml:link rel="alternate" hreflang="en" href="https://localhost/en/articles/article-3-en" />', 1);
     }
 
     private function generateSitemap(): string
--- tests/Helpers/Traits/TemplateCreator.php
+++ tests/Helpers/Traits/TemplateCreator.php
@@ -11,6 +11,18 @@ trait TemplateCreator
         return Template::factory()->create($params);
     }
 
+    private function createPublicTemplate(array $params = []): Template
+    {
+        return Template::factory()->create(array_merge(
+            $params,
+            [
+                'status' => Template::_STATUS_PUBLISHED,
+                'publish_at' => now()->subYear(),
+                'indexation' => true,
+            ]
+        ));
+    }
+
     private function createHomepageTemplate(array $params = []): Template
     {
         return Template::factory()->create(array_merge(
