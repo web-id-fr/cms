aa23c794b2728f507412a41126180117d4dc4c43
Elise <elise@web-id.fr>
2021-04-15 16:07:46 +0200

    :children_crossing: Optimize user experience in nested-rows

--- src/nova-components/MenuItemField/resources/js/components/nested-rows.vue
+++ src/nova-components/MenuItemField/resources/js/components/nested-rows.vue
@@ -8,10 +8,28 @@
         @input="emitter"
     >
         <div class="item-group" :key="el.id" v-for="el in realValue">
-            <div class="item shadow-md p-3 my-2"><font-awesome-icon class="icons" icon="bars"/>
-              {{ selectFirstTitle(el.title) }}
+            <div class="item">
+                <div class="item-title flex min-h-full w-full border-t border-l border-b border-60 rounded-l">
+                    <span>
+                        <font-awesome-icon class="icons" icon="bars"/>
+                        {{ selectFirstTitle(el.title) }}
+                    </span>
+                </div>
+                <div class="item-button z-10 bg-white border-t border-60 border rounded-r-lg h-auto pin-l pin-t self-start w-8">
+                    <div>
+                        <button v-tooltip.click="__('Detach')" @click.prevent="selectMenuItem(el)"
+                                class="group-control btn w-8 h-8 block has-tooltip">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 20 20"
+                                 aria-labelledby="delete" role="presentation" class="fill-current">
+                                <path fill="grey" fill-rule="nonzero"
+                                      d="M6 4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6H1a1 1 0 1 1 0-2h5zM4 6v12h12V6H4zm8-2V2H8v2h4zM8 8a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <nested-rows class="item-sub" :list="mapChildren(el)" />
+
+            <nested-rows v-on:selectMenuItems="selectMenuItem" class="item-sub" :list="mapChildren(el)"/>
         </div>
     </draggable>
 </template>
@@ -39,6 +57,19 @@
             }
         },
 
+        props: {
+            value: {
+                required: false,
+                type: Array,
+                default: null
+            },
+            list: {
+                required: false,
+                type: Array,
+                default: null
+            },
+        },
+
         mounted() {
             this.currentLocale = document.querySelector('#select-language-translatable').value;
             Nova.$on('change-language', (lang) => {
@@ -52,11 +83,11 @@
             },
 
             selectFirstTitle(title) {
-                if(!title[this.currentLocale]) {
-                    if(title[this.currentLocale + 1]) {
+                if (!title[this.currentLocale]) {
+                    if (title[this.currentLocale + 1]) {
                         return title[this.currentLocale + 1];
-                    } else if(title[this.currentLocale - 1]) {
-                        return title[this.currentLocale -1];
+                    } else if (title[this.currentLocale - 1]) {
+                        return title[this.currentLocale - 1];
                     } else {
                         return title[Object.keys(title)[0]];
                     }
@@ -67,7 +98,11 @@
 
             mapChildren(el) {
                 return mapChildren(el);
-            }
+            },
+
+            selectMenuItem(menuItem) {
+                this.$emit('selectMenuItems', menuItem);
+            },
         },
 
         computed: {
@@ -85,19 +120,6 @@
                 return this.value ? this.value : this.list;
             }
         },
-
-        props: {
-            value: {
-                required: false,
-                type: Array,
-                default: null
-            },
-            list: {
-                required: false,
-                type: Array,
-                default: null
-            }
-        }
     };
 </script>
 
@@ -106,13 +128,28 @@
         margin: 0;
     }
     .item {
+        display: flex;
+        margin-bottom: 5px;
+    }
+    .item-title {
+        align-items: center;
+    }
+    span {
         padding: 1rem;
-        margin: 5px;
+    }
+    .item-button div {
+        padding: 1rem 0;
     }
     .item-sub {
         margin: 0 0 0 3rem;
     }
     .icons {
         margin-right: 20px;
+    }
+    .sortable-chosen{
+        border: 1px dashed #0088F8;
+        box-sizing: border-box;
+        background: rgba(0, 136, 249, 0.09);
+        color: #0088f9;
     }
 </style>
