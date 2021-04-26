9ff7eee2189ef785a5ce1b2788ea2baea198446c
Wilfried <wilfried@web-id.fr>
2021-04-26 16:07:44 +0200

    🏷️ Add some missing type hints & phpdoc

--- src/app/Models/Menu/MenuItem.php
+++ src/app/Models/Menu/MenuItem.php
@@ -5,6 +5,14 @@ namespace Webid\Cms\App\Models\Menu;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class MenuItem
+ *
+ * @package Webid\Cms\App\Models\Menu
+ *
+ * @property string $menuable_type
+ * @property int $menu_id
+ */
 class MenuItem extends Model
 {
     use HasFactory;
