8e5392465a7df165b3107990a316ea4f260ea9d0
Elise <elise@web-id.fr>
2021-07-01 11:53:56 +0200

    :sparkles: Add new features for tinyMCE

--- src/nova-components/TranslatableTool/resources/js/Trix.vue
+++ src/nova-components/TranslatableTool/resources/js/Trix.vue
@@ -14,12 +14,30 @@
                 config: {
                     relative_urls: false,
                     remove_script_host: false,
-                    entity_encoding : "raw",
+                    entity_encoding: "raw",
                     encoding: "UTF-8",
                     menubar: false,
-                    plugins: "code textcolor preview link lists",
-                    toolbar: "code | undo redo | formatselect | bold italic strikethrough underline forecolor backcolor | link | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | link",
-                    forced_root_block: "p"
+                    plugins: "code textcolor preview link lists media table image imagetools emoticons charmap",
+                    toolbar: "code | undo redo | formatselect | bold italic strikethrough underline forecolor backcolor | link image media | alignleft aligncenter alignright alignjustify | table | bullist numlist outdent indent blockquote | charmap emoticons | removeformat",
+                    forced_root_block: "p",
+                    automatic_uploads: true,
+                    file_picker_types: 'image file media',
+                    media_live_embeds: true,
+                    image_advtab: true,
+                    file_picker_callback: function (callback, value, meta) {
+                        let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                        let y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+                        let url = '/nova/nova-filemanager';
+                        tinyMCE.activeEditor.windowManager.openUrl({
+                            url: url,
+                            title: 'File manager',
+                            width: x * 0.8,
+                            height: y * 0.8,
+                            onMessage: (api, message) => {
+                                callback(message.content);
+                            }
+                        });
+                    }
                 }
             }
         },
