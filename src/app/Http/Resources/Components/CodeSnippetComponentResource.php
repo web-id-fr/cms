0b4f5ef074bda5fe9e2e21c21de421980ace3d7a
Elise <elise@web-id.fr>
2021-09-03 12:33:59 +0200

    :art: Add new conditions in jsonResource components

--- src/app/Http/Resources/Components/CodeSnippetComponentResource.php
+++ src/app/Http/Resources/Components/CodeSnippetComponentResource.php
@@ -2,6 +2,7 @@
 
 namespace Webid\Cms\App\Http\Resources\Components;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Webid\Cms\App\Models\Components\CodeSnippetComponent;
 use Webid\Cms\Modules\JavaScript\Http\Resources\CodeSnippetResource;
@@ -11,17 +12,19 @@ class CodeSnippetComponentResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      *
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id' => $this->resource->component->id,
-            'name' => $this->resource->component->name,
-            'code_snippet' => CodeSnippetResource::make($this->resource->component->codeSnippet)->resolve(),
-            'view' => config("components." . CodeSnippetComponent::class  .".view"),
+            $this->mergeWhen($this->resource->component->status === CodeSnippetComponent::_STATUS_PUBLISHED, [
+                'id' => $this->resource->component->id,
+                'name' => $this->resource->component->name,
+                'code_snippet' => CodeSnippetResource::make($this->resource->component->codeSnippet)->resolve(),
+                'view' => config("components." . CodeSnippetComponent::class . ".view"),
+            ]),
         ];
     }
 }
