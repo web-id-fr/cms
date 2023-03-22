070c82bb5c63d86e24d3cf162231029951791ad8
LIEGEOIS Elise <elise@web-id.fr>
2023-03-22 11:05:11 +0100

    :bug: Fix sqlite migration

---
+++ src/database/migrations/2023_03_22_091513_create_migrations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMigrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('migrations')) {
+            Schema::create('projects', function (Blueprint $table) {
+                $table->unsignedInteger('id');
+                $table->string('name');
+                $table->integer('batch');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('migrations');
+    }
+}
